9eb440af53d6b6d77bdd32ef1532c09f2f341341
cjsxorud111 <cjsxorud111@gmail.com>
2023-04-29 10:24:16 +0900

    Fix

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -17,8 +17,10 @@ public class OrderApp {
         Member member = new Member(memberId, "memberA", Grade.VIP);
         memberService.join(member);
 
+
         Order order = orderService.createOrder(memberId, "itemA", 10000);
 
         System.out.println("order = " + order);
+
     }
 }
--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -4,7 +4,7 @@ public class MemberServiceImpl implements MemberService{
     private final MemberRepository memberRepository = new
             MemoryMemberRepository();
     public void join(Member member) {
-        memberRepository.save(member);
+       memberRepository.save(member);
     }
     public Member findMember(Long memberId) {
         return memberRepository.findById(memberId);
