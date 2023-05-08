cd657901dc113d04a98bf2d7240cc89468af69c7
cjsxorud111 <cjsxorud111@gmail.com>
2023-05-08 23:35:22 +0900

    Test 추가

--- src/test/java/hello/core/singleton/ConfiguratoinSingletonTest.java
+++ src/test/java/hello/core/singleton/ConfiguratoinSingletonTest.java
@@ -25,4 +25,12 @@ public class ConfiguratoinSingletonTest {
         System.out.println("memberService -> memberRepository = " + memberRepository2);
         System.out.println("memberRepository = " + memberRepository);
     }
+
+    @Test
+    void configurationDeep() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        System.out.println("bean = " + bean.getClass());
+    }
 }
