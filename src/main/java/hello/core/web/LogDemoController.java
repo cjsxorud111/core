a62e82bf7177511ce4c9ffb84cb226c9f12da524
tae <cjsxorud111@gmail.com>
2023-05-29 00:26:02 +0900

    request 요청시에만 빈생성 예제 추가

---
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -0,0 +1,28 @@
+package hello.core.web;
+
+import hello.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+    private final LogDemoService logDemoService;
+    private final ObjectProvider<MyLogger> myLoggerProbider;
+
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logdemo(HttpServletRequest httpServletRequest) {
+        String url = httpServletRequest.getRequestURI().toString();
+        MyLogger myLogger = myLoggerProbider.getObject();
+
+        myLogger.log("controll Test");
+        logDemoService.logic("testID");
+        return "OK";
+    }
+}
